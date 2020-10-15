edc71424b95c444ed675058e1bc71ea7feebe4a6
lalosamuel <cordovaallende@gmail.com>
2020-10-15 01:39:16 -0500

    mantenimiento requisitos

--- mantenimiento/src/app/app.routes.ts
+++ mantenimiento/src/app/app.routes.ts
@@ -3,16 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { InicioComponent } from './inicio/inicio.component';
 import { TipoPrestamoComponent } from './tipo_prestamo/tipo_prestamo.component';
-import { TipoPrestamoFormComponent } from './tipo_prestamo/tipo_prestamo_form/tipo_prestamo_form.component';
+import { RequisitoComponent } from './requisito/requisito.component';
 
 const appRoutes: Routes = [
   /*{path: '', redirectTo: '/productos', pathMatch: 'full'},*/
   {path: '', component: InicioComponent},
   {path: 'tipo_prestamo', component: TipoPrestamoComponent},
-  {path: 'tipo_prestamo/create', component: TipoPrestamoFormComponent},
-  {path: 'tipo_prestamo/edit/:idProducto', component: TipoPrestamoFormComponent},
-  /*{path: 'ventas', component: VentaComponent},
-  {path: 'ventas/create/:idProducto', component: CrearVentaComponent},
+  {path: 'requisito:idTipoPrestamo', component: RequisitoComponent},
+
+
+  /*{path: 'ventas/create/:idProducto', component: CrearVentaComponent},
   {path: 'ventas/edit/:idVentas', component: EditarVentaComponent},
   {path: 'ventas/delete/:idVentas', component: EliminarVentaComponent},
   {path: 'producciones', component: ProduccionComponent},
