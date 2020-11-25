aab428492a2837b858826e3e1e93fc167e761854
lalosamuel <cordovaallende@gmail.com>
2020-11-25 00:49:42 -0500

    ver prestamos por cliente

---
+++ mantenimiento/app.js
@@ -0,0 +1,121 @@
+'use strict';
+
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const app = express();
+const server = http.Server(app); //createServer
+const debug = require('debug')('myapp:server');
+
+const COOKIE_SECRET = 'godialoginq';
+const COOKIE_NAME = 'godialoginq_sid';
+
+var session_config = {
+    name: COOKIE_NAME,
+    secret: COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { }
+}
+
+if (app.get('env') === 'production') {
+    app.set('trust proxy', 1) // trust first proxy
+    session_config.cookie.secure = true // serve secure cookies
+}
+
+const sessionMiddleware = session(session_config);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+var port = normalizePort(process.env.PORT || '8082');
+app.set('port', port);
+
+app.use(cookieParser());
+app.use(sessionMiddleware);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//imports
+const index = require('./routes/index');
+
+//routes
+app.use(index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('Ha ocurrido un error');
+});
+
+server.listen(port, () => {
+    console.log('Server on Port: ' + port)
+});
+
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+
+    if (port >= 0) {
+        return port;
+    }
+
+    return false;
+}
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
+
+module.exports = app;
