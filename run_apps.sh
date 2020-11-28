7b5c3bd21eee06aac48b464ed6a4f49bb7216507
lalosamuel <cordovaallende@gmail.com>
2020-11-27 23:10:36 -0500

    Cambiando configuracion de run apps

--- run_apps.sh
+++ run_apps.sh
@@ -3,10 +3,10 @@
 COMPOSE_ID=${JOB_NAME:-local}
 
 # Remove Previous Stack
-#docker-compose -p $COMPOSE_ID rm -f
+docker-compose -p $COMPOSE_ID rm -f
 
 # Remove Previous Stack
-docker-compose -p $COMPOSE_ID down
+#docker-compose -p $COMPOSE_ID down
 
 # Starting new stack environment
 docker-compose -p $COMPOSE_ID up -d --build
